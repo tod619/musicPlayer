9abebd408a1fbfaba97c338bbbe92eb7c523c1e6
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-04-18 13:54:45 +0100

    Add eventListners onto the pre + next btns

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 // song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = [ 'hey', 'summer', 'ukulele' ];
 
 // Keep track of song
 let songIndex = 2;
@@ -27,37 +27,39 @@ loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.jpg`;
+	title.innerText = song;
+	audio.src = `music/${song}.mp3`;
+	cover.src = `images/${song}.jpg`;
 }
 
 // Play song
 function playSong() {
-    musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-pause');
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
 
-    audio.play();
+	audio.play();
 }
 
 // Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-pause');
-    play.querySelector('i.fas').classList.add('fa-play');
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
+	play.querySelector('i.fas').classList.add('fa-play');
 
-    audio.pause();
+	audio.pause();
 }
-
 
 // EventListners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
+	const isPlaying = musicContainer.classList.contains('play');
 
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
+	if (isPlaying) {
+		pauseSong();
+	} else {
+		playSong();
+	}
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);

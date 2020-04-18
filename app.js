b5513fb9ae5680204ab444359f35a33b77554efc
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-04-18 10:53:32 +0100

    add variables in app.js file

---
+++ app.js
@@ -0,0 +1,15 @@
+// DOM variables
+const musicContainer = document.getElementById('music_container');
+
+// buttons
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+// Progress bar
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress_container');
+
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');

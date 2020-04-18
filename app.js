cdbe7a85b707867df00a7b6155076252cca6235b
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-04-18 14:21:08 +0100

    Add eventListner to progressContainer

--- app.js
+++ app.js
@@ -100,3 +100,6 @@ nextBtn.addEventListener('click', nextSong);
 
 // Time Song Update
 audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);

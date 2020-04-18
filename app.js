4e5691f60cc13622dbe0e7099259fcaec6c1a281
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-04-18 11:15:27 +0100

    add playSong function in app.js file

--- app.js
+++ app.js
@@ -31,3 +31,26 @@ function loadSong(song) {
     audio.src = `music/${song}.mp3`;
     cover.src = `images/${song}.jpg`;
 }
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+// Pause song
+
+
+// EventListners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
